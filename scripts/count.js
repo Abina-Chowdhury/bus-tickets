507e90dbcdf75449dfda59a6e7f824f9252cb389
Abina Chowdhury <abina@etlimited.net>
2024-02-19 23:35:11 +0600

    create project

---
+++ scripts/count.js
@@ -0,0 +1,47 @@
+
+function A1() {
+    const btnSection = document.getElementById('btn-one');
+    btnSection.classList.add('bg-green-500');
+
+}
+
+function A2() {
+    const btnSection = document.getElementById('btn-two');
+    btnSection.classList.add('bg-green-500');
+
+}
+function A3() {
+    const btnSection = document.getElementById('btn-three');
+    btnSection.classList.add('bg-green-500');
+
+}
+function A4() {
+    const btnSection = document.getElementById('btn-four');
+    btnSection.classList.add('bg-green-500');
+
+}
+function B1() {
+    const btnSection = document.getElementById('btn-five');
+    btnSection.classList.add('bg-green-500');
+
+}
+function B2() {
+    const btnSection = document.getElementById('btn-six');
+    btnSection.classList.add('bg-green-500');
+
+}
+function B3() {
+    const btnSection = document.getElementById('btn-seven');
+    btnSection.classList.add('bg-green-500');
+
+}
+function B4() {
+    const btnSection = document.getElementById('btn-eight');
+    btnSection.classList.add('bg-green-500');
+
+}
+
+
+
+
+
